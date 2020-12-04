de3ebb86cd09c2f044fc59c2a136b6e37b419bec
General-Mudkip <bencethegreat@gmail.com>
2020-12-04 10:30:21 +0000

    Added number guesser button to the RPG window

--- RPG/RPG.py
+++ RPG/RPG.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from tkinter import font as tkFont
 from tictactoeGUI import ticTacToe as tttGame
+from numberGuesser import guessingGame as gGame
 
 # Initializes main window
 root = tk.Tk()
@@ -18,6 +19,7 @@ heart1 = tk.Label(root, image = heart_img)
 heart2 = tk.Label(root, image = heart_img)
 heart3 = tk.Label(root, image = heart_img)
 tttButton = tk.Button(root, text = "Play Tic Tac Toe", command = tttGame)
+ggButton = tk.Button(root, text = "Play Number Guesser", command = gGame)
 
 
 # Displays the created widgets
@@ -25,5 +27,6 @@ heart1.grid(row = 0, column = 0)
 heart2.grid(row = 0, column = 1)
 heart3.grid(row = 0, column = 2)
 tttButton.grid(row = 1, column = 0, columnspan = 3)
+ggButton.grid(row=2, column = 0, columnspan = 3)
 
 root.mainloop() # Starts main window
